5f3d28f41eb9c4e7b3e40ad8e9ce44aad83969dd
Mateusz Bialowas <58574619+mateuszbialowas@users.noreply.github.com>
2023-02-19 12:30:11 +0100

    Text field (#3)
    * Update readme.
    
    * WIP INPUT
    
    * Add text field.
    
    * remove stimulus from text field
    
    * skip testing components.
    
    * add redis to procfile

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Playground
 
-## Generate view components
+## Setup
+Run project in development mode 
+
+`./bin/dev`
+
+### Generate view components
 ```
 bin/rails generate component Example title --preview
+```
+
+### Generate benchmark
+```
+rails g benchmark transactins example1 example2
 ```
---
+++ app/components/shared/inputs/text_field_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Shared
+  module Inputs
+    class TextFieldComponent < ViewComponent::Base
+      include InlineSvg::ActionView::Helpers
+
+      def initialize(leading_icon: nil, label: true, validation: nil, hint_text: nil)
+        @leading_icon = leading_icon
+        @label = label
+        @validation = validation
+        @hint_text = hint_text
+      end
+
+      private
+
+      attr_reader :leading_icon, :label, :validation, :hint_text
+    end
+  end
+end
